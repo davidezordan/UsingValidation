3f09e323ca1b2fc01e3a46e4cbef7bfbc5a3e6d9
Davide Zordan <davide.zordan@microsoft.com>
2017-02-28 14:21:11 +0000

    Minor changes

--- UsingValidation/UsingValidation/ViewModels/MainPageViewModel.cs
+++ UsingValidation/UsingValidation/ViewModels/MainPageViewModel.cs
@@ -11,13 +11,6 @@ namespace UsingValidation.ViewModels
             DemoItem = new Item();
         }
 
-        private string _title;
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
-
         private Item _item;
         public Item DemoItem
         {
@@ -27,14 +20,11 @@ namespace UsingValidation.ViewModels
 
         public void OnNavigatedFrom(NavigationParameters parameters)
         {
-
         }
 
         public void OnNavigatedTo(NavigationParameters parameters)
         {
-            if (parameters.ContainsKey("title"))
-                Title = (string)parameters["title"] + " and Prism";
-
+            DemoItem.Name = string.Empty;
             DemoItem.Description = string.Empty;
         }
     }
