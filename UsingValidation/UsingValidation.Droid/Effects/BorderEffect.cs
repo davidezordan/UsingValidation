4d72384710be45b49c376dafdba2ad7d27fa4108
Paul Datsiuk <jthotbox@gmail.com>
2017-10-09 13:11:21 +0300

    add border effects for ios and android

---
+++ UsingValidation/UsingValidation.Droid/Effects/BorderEffect.cs
@@ -0,0 +1,36 @@
+﻿using Android.Support.V4.Content;
+using System;
+using UsingValidation.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("UsingValidationSample")]
+[assembly: ExportEffect(typeof(BorderEffect), "BorderEffect")]
+namespace UsingValidation.Droid.Effects
+{
+    public class BorderEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                var drawable = ContextCompat.GetDrawable(Control.Context, Resource.Drawable.border);
+                Control.SetBackground(drawable);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            try
+            {
+                Control.SetBackground(null);
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
